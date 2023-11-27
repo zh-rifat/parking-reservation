6401af8ff8e3995a1bfc1eb97187a1cae81cac78
zh-rifat <zhrifat.00@gmail.com>
2023-11-28 02:42:18 +0600

    backend init

---
+++ backend/api/index.js
@@ -0,0 +1,17 @@
+
+import express from 'express';
+import cors from 'cors';
+import { getAllCells } from '../routes/read.js';
+import { deleteData, writeData } from '../routes/write.js';
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.listen(3000 || process.env.PORT, () => {
+  console.log('Up and running!!');
+});
+app.get('/api/readallcells',getAllCells);
+app.post('/api/writecell',writeData);
+app.post('/api/deletecell',deleteData);
+
+export default app;
