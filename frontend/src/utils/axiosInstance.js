65436d6b738eefd84c1079cf94c94c1bdc05db02
zh-rifat <zhrifat.00@gmail.com>
2023-11-28 02:50:44 +0600

    backend init

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,8 +1,11 @@
 import axios from "axios";
+
 // axios.defaults.baseURL="http://localhost:5000";
+const backendUri=import.meta.env.VITE_BACKEND_API;
+console.log(backendUri)
 const instance = axios.create({
     // baseURL: "http://localhost:3000/api"
-    baseURL:process.env.REACT_APP_BACKEND_API
+    baseURL: backendUri
   });
   
 export default instance;
